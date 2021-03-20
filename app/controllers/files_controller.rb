97c573611051a22e09dc9685168609747c8c633e
qytiz <66452104+qytiz@users.noreply.github.com>
2021-03-20 11:56:30 +0300

    Update files_controller.rb

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -3,7 +3,7 @@
 class FilesController < ApplicationController
   def destroy
     @file = ActiveStorage::Attachment.find(params[:id])
-    @resource = @file.record_type.constantize.find(@file.record_id)
+    @resource = @file.record_id
     @file.purge if current_user&.author?(@resource)
   end
 end
